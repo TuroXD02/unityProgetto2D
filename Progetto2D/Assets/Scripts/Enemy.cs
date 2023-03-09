545e268f849709d2ba9a236f1a54ba3e9e8df4d8
TuroXD02 <davide.turini.dt@gmail.com>
2023-03-09 20:40:54 +0100

    lion vitale

---
+++ Progetto2D/Assets/Scripts/Enemy.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Enemy : MonoBehaviour
+{
+    public List<Transform> points;
+    public int nextID = 0;
+    int idChangeValue = 1;
+    public float speed = 2f;
+
+    void Reset()
+    {
+        Init();
+    }
+
+    void Update()
+    {
+        MoveToNextPoint();
+    }
+
+    void Init()
+    {
+
+        GetComponent<BoxCollider2D>().isTrigger = true;
+        //creazione root nemico
+        GameObject root = new GameObject(name + "_Root");
+        root.transform.position = transform.position;
+        transform.SetParent(root.transform);
+        //creazione root waypoints
+        GameObject waypoints = new GameObject("Waypoints");
+        waypoints.transform.SetParent(root.transform);
+        //creazione waypoints
+        GameObject p1 = new GameObject("Point 1"); p1.transform.SetParent(waypoints.transform);
+        GameObject p2 = new GameObject("Point 2"); p2.transform.SetParent(waypoints.transform);
+        p1.transform.position = transform.position;
+        p2.transform.position = transform.position;
+        //aggiungi points alla lista
+        points = new List<Transform>();
+        points.Add(p1.transform);
+        points.Add(p2.transform);
+    }
+
+    void MoveToNextPoint()
+    {
+        Transform goalPoint = points[nextID];
+        if(goalPoint.transform.position.x > transform.position.x)
+            transform.localScale = new Vector3(4, 4, 1);
+        else
+            transform.localScale = new Vector3(-4, 4, 1);
+
+        transform.position = Vector2.MoveTowards(transform.position,goalPoint.position, speed *Time.deltaTime);
+
+        if(Vector2.Distance(transform.position, goalPoint.position)<.2f)
+        {
+            if (nextID == points.Count - 1)
+                idChangeValue = -1;
+            if (nextID == 0)
+                idChangeValue = 1;
+            nextID += idChangeValue;
+        }
+    }
+
+}
